8de7c8084f3a46f2c6bb630e3deba1cd6d99f8b3
Seb <gillisl22@outlook.com>
2022-06-17 19:32:12 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = 3001;
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+// Html Routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+
+// getting the notes info
+app.get('api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './db/db.json'));
+});
+
+// using the notes info
+app.post ('api/notes', (req, res) => {
+  const { title, text } = req.body;
+
+  if (title, text) {
+    const newNotes = {
+      title,
+      text,
+    }
+
+  const response = {
+    status: 'success',
+    body: newNotes,
+  };
+
+  console.log(response);
+  res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting notes');
+  }
+});
+
+
+
+
+// making the have a url
+app.listen(PORT, () =>
+  console.log(`Wesbite hosted at http://localhost:${PORT}`)
+);
