4dfb2cf57697296be9a6164a5ce25ef9f75924a3
Seb <gillisl22@outlook.com>
2022-06-17 20:36:34 -0400

    ports fixs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const PORT = 3001;
+const PORT = process.envPORT || 3001;
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
